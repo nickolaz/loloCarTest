1c83f18890bf7e7642f012736ca2ce25877ac462
Nicolas Ayala <nicolas.ayala@mangoapp.com.py>
2022-12-19 17:56:35 -0300

    crear una aplicación móvil usando React Native CLI

---
+++ loloCarTest/App.tsx
@@ -0,0 +1,99 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const [input, setInput] = useState('');
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <View style={styles.headerStyle}>
+            <Text style={styles.sectionTitle}>Welcome to Lolocar</Text>
+          </View>
+          <View style={{paddingVertical: 20}}>
+            <Text style={styles.bodyTitle}>What's your name?</Text>
+            <TextInput
+              value={input}
+              onChangeText={e => {
+                setInput(e);
+              }}
+              style={{backgroundColor: 'gray', color: 'white'}}
+            />
+          </View>
+          {input.length > 3 && (
+            <View style={{paddingVertical: 10}}>
+              <Text style={styles.bodyTitle}>{`Hola,${input}!`}</Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'white',
+  },
+  bodyTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+    // color: 'white',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  headerStyle: {
+    backgroundColor: 'gray',
+    paddingTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
